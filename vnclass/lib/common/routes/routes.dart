800124616aa3ca1d49b32bdba584a0b71277cd96
Do Suong <92018040+dosuong@users.noreply.github.com>
2025-03-20 16:53:25 +0700

    đá

--- vnclass/lib/common/routes/routes.dart
+++ vnclass/lib/common/routes/routes.dart
@@ -1,4 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
+
+import 'package:vnclass/web/account/account_create_acc_page_web.dart'
+    if (kIsWeb) 'package:vnclass/web/account/account_create_acc_page_web.dart';
+import 'package:vnclass/web/account/account_edit_acc_page_web.dart'
+    if (kIsWeb) 'package:vnclass/web/account/account_edit_acc_page_web.dart';
+import 'package:vnclass/web/account/account_main_page_web.dart'
+    if (kIsWeb) 'package:vnclass/web/account/account_main_page_web.dart';
+// Import các tệp dành cho mobile
 import 'package:vnclass/modules/account/view/account_creat_acc_page.dart';
 import 'package:vnclass/modules/account/view/account_edit_acc_page.dart';
 import 'package:vnclass/modules/account/view/account_main_page.dart';
@@ -25,9 +34,37 @@ import 'package:vnclass/modules/notification/view/notification_screen.dart';
 import 'package:vnclass/modules/report/view/report_main_page.dart';
 import 'package:vnclass/modules/search/search_screen.dart';
 import 'package:vnclass/modules/splash_screen/slpash_screen.dart';
-import 'package:vnclass/web/home_view_web.dart';
+
+// Import các tệp dành cho web (chỉ khi kIsWeb là true)
+
+import 'package:vnclass/web/home_view_web.dart'
+    if (kIsWeb) 'package:vnclass/web/home_view_web.dart';
+import 'package:vnclass/web/login/login_web_page.dart'
+    if (kIsWeb) 'package:vnclass/web/login/login_web_page.dart';
+import 'package:vnclass/web/main_home/main_home_web_page.dart'
+    if (kIsWeb) 'package:vnclass/web/main_home/main_home_web_page.dart';
+import 'package:vnclass/web/mistake/mistake_class_detail_page_web.dart'
+    if (kIsWeb) 'package:vnclass/web/mistake/mistake_class_detail_page_web.dart';
+import 'package:vnclass/web/mistake/mistake_edit_mistake_page_web.dart'
+    if (kIsWeb) 'package:vnclass/web/mistake/mistake_edit_mistake_page_web.dart';
+import 'package:vnclass/web/mistake/mistake_main_page_web.dart'
+    if (kIsWeb) 'package:vnclass/web/mistake/mistake_main_page_web.dart';
+import 'package:vnclass/web/mistake/mistake_type_mistake_page_web.dart'
+    if (kIsWeb) 'package:vnclass/web/mistake/mistake_type_mistake_page_web.dart';
+import 'package:vnclass/web/mistake/mistake_view_mistake_page_web.dart'
+    if (kIsWeb) 'package:vnclass/web/mistake/mistake_view_mistake_page_web.dart';
+import 'package:vnclass/web/mistake/mistake_write_mistake_page_web.dart'
+    if (kIsWeb) 'package:vnclass/web/mistake/mistake_write_mistake_page_web.dart';
+import 'package:vnclass/web/set_up_mistake/user_change_type_mistake_screen_web.dart'
+    if (kIsWeb) 'package:vnclass/web/set_up_mistake/user_change_type_mistake_screen_web.dart';
+import 'package:vnclass/web/set_up_point/user_set_points_screen_web.dart'
+    if (kIsWeb) 'package:vnclass/web/set_up_point/user_set_points_screen_web.dart';
+import 'package:vnclass/web/setting/user_screen_web.dart'
+    if (kIsWeb) 'package:vnclass/web/setting/user_screen_web.dart';
 
 final Map<String, WidgetBuilder> routes = {
+  // Routes chung cho cả mobile và web
+  SplashScreen.routeName: (context) => const SplashScreen(),
   LoginPage.routeName: (context) => const LoginPage(),
   MainHomePage.routeName: (context) => const MainHomePage(),
   MistakeMainPage.routeName: (context) => const MistakeMainPage(),
@@ -51,13 +88,36 @@ final Map<String, WidgetBuilder> routes = {
   AllConduct.routeName: (context) => const AllConduct(),
   ConductDetail.routeName: (context) => const ConductDetail(),
   StudentConductInfo.routeName: (context) => const StudentConductInfo(),
-  LoginPage.routeName: (context) => const LoginPage(),
-  MainHomePage.routeName: (context) => const MainHomePage(),
   NotificationScreen.routeName: (context) => const NotificationScreen(),
   SearchScreen.routeName: (context) => const SearchScreen(),
   StudentConductInfoMonth.routeName: (context) =>
       const StudentConductInfoMonth(),
-  HomeViewWeb.routeName: (context) => const HomeViewWeb(),
-  SplashScreen.routeName: (context) => const SplashScreen(),
   UserSetPointsScreen.routeName: (context) => const UserSetPointsScreen(),
+
+  // Routes chỉ dành cho web (được thêm khi kIsWeb là true)
+  if (kIsWeb) ...{
+    AccountMainPageWeb.routeName: (context) => const AccountMainPageWeb(),
+    AccountCreateAccPageWeb.routeName: (context) =>
+        const AccountCreateAccPageWeb(),
+    AccountEditAccPageWeb.routeName: (context) => const AccountEditAccPageWeb(),
+    HomeViewWeb.routeName: (context) => const HomeViewWeb(),
+    LoginWebPage.routeName: (context) => const LoginWebPage(),
+    MainHomeWebPage.routeName: (context) => const MainHomeWebPage(),
+    MistakeMainPageWeb.routeName: (context) => const MistakeMainPageWeb(),
+    MistakeClassDetailPageWeb.routeName: (context) =>
+        const MistakeClassDetailPageWeb(),
+    MistakeTypeMistakePageWeb.routeName: (context) =>
+        const MistakeTypeMistakePageWeb(),
+    MistakeWriteMistakePageWeb.routeName: (context) =>
+        const MistakeWriteMistakePageWeb(),
+    UserScreenWeb.routeName: (context) => const UserScreenWeb(),
+    UserChangeTypeMistakeScreenWeb.routeName: (context) =>
+        const UserChangeTypeMistakeScreenWeb(),
+    UserSetPointsScreenWeb.routeName: (context) =>
+        const UserSetPointsScreenWeb(),
+    MistakeViewMistakePageWeb.routeName: (context) =>
+        const MistakeViewMistakePageWeb(),
+    MistakeEditMistakePageWeb.routeName: (context) =>
+        const MistakeEditMistakePageWeb(),
+  },
 };
--- vnclass/lib/main.dart
+++ vnclass/lib/main.dart
@@ -1,23 +1,42 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:vnclass/common/design/color.dart';
+import 'package:vnclass/common/helper/local_storage_helper.dart';
+import 'package:vnclass/common/providers/class_mistake_provider.dart';
 import 'package:vnclass/common/routes/routes.dart';
+import 'package:vnclass/firebase_options.dart';
 import 'package:vnclass/modules/login/controller/provider.dart';
 import 'package:vnclass/modules/main_home/controller/class_provider.dart';
+import 'package:vnclass/modules/main_home/controller/permission_provider.dart';
+import 'package:vnclass/modules/main_home/controller/student_detail_provider.dart';
+import 'package:vnclass/modules/main_home/controller/teacher_provider.dart';
 import 'package:vnclass/modules/main_home/controller/year_provider.dart';
 import 'package:vnclass/modules/notification/funtion/notification_change.dart';
+import 'package:vnclass/modules/notification/funtion/notification_service.dart';
 import 'package:vnclass/modules/splash_screen/slpash_screen.dart';
 import 'package:vnclass/web/home_view_web.dart';
+import 'package:vnclass/web/login/login_web_page.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize services
+  await LocalStorageHelper.initLocalStorageHelper();
+  await NotificationService.initialize();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (context) => AccountProvider()),
         ChangeNotifierProvider(create: (_) => YearProvider()),
         ChangeNotifierProvider(create: (_) => ClassProvider()),
+        ChangeNotifierProvider(create: (_) => PermissionProvider()),
+        ChangeNotifierProvider(create: (_) => TeacherProvider()),
+        ChangeNotifierProvider(create: (_) => StudentDetailProvider()),
+        ChangeNotifierProvider(create: (_) => ClassMistakeProvider()),
         ChangeNotifierProvider(create: (context) => NotificationChange()),
       ],
       child: const MyApp(),
@@ -37,7 +56,7 @@ class MyApp extends StatelessWidget {
         primaryColor: ColorApp.primaryColor,
       ),
       routes: routes,
-      initialRoute: kIsWeb ? HomeViewWeb.routeName : SplashScreen.routeName,
+      initialRoute: kIsWeb ? LoginWebPage.routeName : SplashScreen.routeName,
     );
   }
 }
